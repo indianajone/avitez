161685621e4c84e390b6c33faf38656a36323246
indianajone <mmer555@hotmail.com>
2015-06-08 12:10:29 +0700

    Config

--- config/database.php
+++ config/database.php
@@ -55,9 +55,9 @@ return [
 		'mysql' => [
 			'driver'    => 'mysql',
 			'host'      => env('DB_HOST', 'localhost'),
-			'database'  => env('DB_DATABASE', 'forge'),
-			'username'  => env('DB_USERNAME', 'forge'),
-			'password'  => env('DB_PASSWORD', ''),
+			'database'  => env('DB_DATABASE', 'forge'), // avitez_db
+			'username'  => env('DB_USERNAME', 'forge'), // avitez_admin
+			'password'  => env('DB_PASSWORD', ''), // Bv0Zg0eRri
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
